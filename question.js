c32fe688a1165b02336ede8157be32657149544f
Husaam NT <husnaeem@gmail.com>
2022-12-03 21:44:33 +0000

    added question.js and question1 new class

---
+++ question.js
@@ -0,0 +1,13 @@
+class Question{
+    constructor(questionString, button1, button2, button3, button4, answer, input){
+        this.questionString = questionString;
+        this.button1 = button1;
+        this.button2 = button2;
+        this.button3 = button3;
+        this.button4 = button4;
+        this.answer = answer;
+        this.input = input;
+    }
+}
+
+module.exports = Question;
---
+++ script.js
@@ -0,0 +1,12 @@
+const Question = require("./question");
+const button1 = $("#option1");
+
+const Questions = new Question(
+  "What does CSS stand for?",
+  "Creative Style Sheet",
+  "Cascading Style Sheet",
+  "Creative Sheet Style",
+  "Concordant Style Shape",
+  this.button2,
+  element.addEventListener("click", this.input)
+);

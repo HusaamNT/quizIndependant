3be1193bfa9faa6fe788daf4d5058ec34beec9b9
Husaam NT <husnaeem@gmail.com>
2022-12-07 16:48:12 +0000

    added button subclasses

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const button1 = $("#option1");
+const button2 = $("#option2");
+const button3 = $("#option3");
+const button4 = $("#option4");
+
 class Question{
   constructor(questionString, answer){
       this.questionString = questionString;
@@ -6,24 +11,39 @@ class Question{
 }
 
 class Button{
-  constructor(answer, id){
-    this.answer = answer;
+  constructor(option, id){
+    this.option = option;
     this.id = id;
   }
 }
-
-const button1 = $("#option1");
-const button2 = $("#option2");
-const button3 = $("#option3");
-const button4 = $("#option4");
+class Button1 extends Button{
+  constructor(option){
+    super(option, id);
+    this.id = "b1";
+  }
+}
+class Button2 extends Button{
+  constructor(option){
+    super(option, id);
+    this.id = "b2";
+  }
+}
+class Button3 extends Button{
+  constructor(option){
+    super(option, id);
+    this.id = "b3";
+  }
+}
+class Button4 extends Button{
+  constructor(option){
+    super(option, id);
+    this.id = "b4";
+  }
+}
 
 const questions1 = new Question(
   "What does CSS stand for?",
-  "Creative Style Sheet",
-  "Cascading Style Sheet",
-  "Creative Sheet Style",
-  "Concordant Style Shape",
-  this.button2,
+
   
 );
 

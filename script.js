bfbcf3a8db3114d6f641cf2426ac144ad0112413
Husaam NT <husnaeem@gmail.com>
2022-12-07 17:05:30 +0000

    added button2,3 and 4Lists constants

--- script.js
+++ script.js
@@ -44,13 +44,31 @@ class Button4 extends Button{
 const questionList = [
   new Question("Which option isn't a data type?", "b2"),
   new Question("Which option is a popular programming language?", "b3"),
-  new Question("Which of these examples aren't a for loop?", "b1"),
+  new Question("Which of these examples aren't a type of for-loop?", "b1"),
   new Question("What is recursion?", "b4")
 ]
 
-const Button1List = [
+const button1List = [
   new Button1("String"),
   new Button1("BananaPeel v3.1"),
+  new Button1("for(each one from all)"),
+  new Button1("A preogramming language")
+]
+const button2List = [
+  new Button1("Word"),
+  new Button1("King Cobra"),
+  new Button1("for(i=0, i<source, i++)"),
+  new Button1("A preogramming language")
+]
+const button3List = [
+  new Button1("Integer"),
+  new Button1("Python"),
   new Button1("for(each element of source)"),
+  new Button1("A preogramming language")
+]
+const button4List = [
+  new Button1("Character"),
+  new Button1("Tarantula"),
+  new Button1("for(let element in source)"),
   new Button1("A preogramming language")
 ]

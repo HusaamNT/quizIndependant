c60605d6054b9934262754d408e88497529122bb
Husaam NT <husnaeem@gmail.com>
2022-12-11 18:50:57 +0000

    changed syntax on element ids js

--- script.js
+++ script.js
@@ -1,89 +1,95 @@
-const button1 = $("#option1");
-const button2 = $("#option2");
-const button3 = $("#option3");
-const button4 = $("#option4");
-const startOption = $("#start")
-const startButtons = $(".startButtons")
+const button1 = document.getElementById("option1");
+const button2 = document.getElementById("option2");
+const button3 = document.getElementById("option3");
+const button4 = document.getElementById("option4");
+const startOption = document.getElementById("start");
+const startButtons = document.getElementsByClassName("startButtons");
 
 $(button1).hide();
 $(button2).hide();
 $(button3).hide();
 $(button4).hide();
 
-class Question{
-  constructor(questionString, answer){
-      this.questionString = questionString;
-      this.answer = answer;
+class Question {
+  constructor(questionString, answer) {
+    this.questionString = questionString;
+    this.answer = answer;
   }
 }
 
-class Button{
-  constructor(option){
+class Button {
+  constructor(option) {
     this.option = option;
   }
 }
-class Button1 extends Button{
-  constructor(option){
+class Button1 extends Button {
+  constructor(option) {
     super(option);
     this.id = "b1";
   }
 }
-class Button2 extends Button{
-  constructor(option){
+class Button2 extends Button {
+  constructor(option) {
     super(option);
     this.id = "b2";
   }
 }
-class Button3 extends Button{
-  constructor(option){
+class Button3 extends Button {
+  constructor(option) {
     super(option);
     this.id = "b3";
   }
 }
-class Button4 extends Button{
-  constructor(option){
+class Button4 extends Button {
+  constructor(option) {
     super(option);
     this.id = "b4";
   }
 }
 
-const question1 =  new Question("Which option isn't a data type?", "b2");
-const question2 =  new Question("Which option is a popular programming language?", "b3");
-const question3 =  new Question("Which of these examples aren't a type of for-loop?", "b1");
-const question4 =  new Question("What is recursion?", "b4");
+const question1 = new Question("Which option isn't a data type?", "b2");
+const question2 = new Question(
+  "Which option is a popular programming language?",
+  "b3"
+);
+const question3 = new Question(
+  "Which of these examples aren't a type of for-loop?",
+  "b1"
+);
+const question4 = new Question("What is recursion?", "b4");
 
 const button1List = [
   new Button1("String"),
   new Button1("BananaPeel v3.1"),
   new Button1("for(each one from all)"),
-  new Button1("A pregramming language")
-]
+  new Button1("A pregramming language"),
+];
 const button2List = [
   new Button1("Word"),
   new Button1("King Cobra"),
   new Button1("for(i=0, i<source, i++)"),
-  new Button1("A preogramming language")
-]
+  new Button1("A preogramming language"),
+];
 const button3List = [
   new Button1("Integer"),
   new Button1("Python"),
   new Button1("for(each element of source)"),
-  new Button1("A preogramming language")
-]
+  new Button1("A preogramming language"),
+];
 const button4List = [
   new Button1("Character"),
   new Button1("Tarantula"),
   new Button1("for(let element in source)"),
-  new Button1("A preogramming language")
-]
+  new Button1("A preogramming language"),
+];
 
-function startGame(){
+const startGame = function() {
   $(startButtons).hide();
   $(button1).show();
-$(button2).show();
-$(button3).show();
-$(button4).show();
-console.log("hi")
-};
+  $(button2).show();
+  $(button3).show();
+  $(button4).show();
+  console.log("hi");
+}
 
-startOption.on("click", startGame())
+startOption.addEventListener("click", startGame)

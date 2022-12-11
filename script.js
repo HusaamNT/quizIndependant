15c4b25b8e7264fa5a822bb854e68cb41823cf3d
Husaam NT <husnaeem@gmail.com>
2022-12-11 20:37:09 +0000

    added innerhtml for q1 buttons

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const button3 = document.getElementById("option3");
 const button4 = document.getElementById("option4");
 const startOption = document.getElementById("start");
 const startButtons = document.getElementsByClassName("startButtons");
+const allOptionButton = document.getElementsByClassName("optionButtons")
 
 $(button1).hide();
 $(button2).hide();
@@ -58,45 +59,44 @@ const question3 = new Question(
 );
 const question4 = new Question("What is recursion?", "b4");
 
+const b1_1 = new Button1("String");
+const b1_2 = new Button1("BananaPeel v3.1");
+const b1_3 = new Button1("for(each one from all)");
+const b1_4 = new Button1("A pregramming language");
 
-const b1_1 =  new Button1("String");
-const b1_2 =  new Button1("BananaPeel v3.1");
-const b1_3 =  new Button1("for(each one from all)");
-const b1_4 =  new Button1("A pregramming language");
+const b2_1 = new Button2("Word");
+const b2_2 = new Button2("King Cobra");
+const b2_3 = new Button2("for(i=0, i<source, i++)");
+const b2_4 = new Button2("A preogramming language");
 
+const b3_1 = new Button3("Integer");
+const b3_2 = new Button3("Python");
+const b3_3 = new Button3("for(each element of source)");
+const b3_4 = new Button3("A preogramming language");
 
-const b2_1 =  new Button1("Word");
-const b2_2 =  new Button1("King Cobra");
-const b2_3 =  new Button1("for(i=0, i<source, i++)");
-const b2_4 =  new Button1("A preogramming language");
+const b4_1 = new Button4("Character");
+const b4_2 = new Button4("Tarantula");
+const b4_3 = new Button4("for(let element in source)");
+const b4_4 = new Button4("A preogramming language");
 
+setUpQuestions = () => {
+  button1.innerHTML = b1_1.option
+  button2.innerHTML = b2_1.option
+  button3.innerHTML = b3_1.option
+  button4.innerHTML = b4_1.option
+};
 
-  const b3_1 =  new Button1("Integer");
-  const b3_2 =  new Button1("Python");
-  const b3_3 =  new Button1("for(each element of source)");
-  const b3_4 =  new Button1("A preogramming language");
-
-
-  const b4_1 = new Button1("Character");
-  const b4_2 =  new Button1("Tarantula");
-  const b4_3 = new Button1("for(let element in source)");
-  const b4_4 = new Button1("A preogramming language");
-
-
-setUpQuestion1 = () => {
-  
-  JSON.stringify(button1List)
-  console.log(typeof button1List)
-  button1.innerHTML = button1List[1]
-}
-setUpQuestion1()
-const startGame = function() {
+const startGame = function () {
   $(startButtons).hide();
   $(button1).show();
   $(button2).show();
   $(button3).show();
   $(button4).show();
-  ;
-}
+  setUpQuestion1();
+};
+  
 
-startOption.addEventListener("click", startGame)
+
+startOption.addEventListener("click", startGame);
+
+allOptionButton.addEventListener("click", )

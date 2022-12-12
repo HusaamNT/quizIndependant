a1ae7029dbbf0abb322d118f0737c9296f77ccab
Husaam NT <husnaeem@gmail.com>
2022-12-12 18:19:57 +0000

    added for loop for all repeated question functions and function isntead of value in the addEventListnerfunction

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+const buttons = [1, 2, 3, 4].map(it => document.getElementById(`option${it}`))
+
+/*
 const button1 = document.getElementById("option1");
 const button2 = document.getElementById("option2");
 const button3 = document.getElementById("option3");
 const button4 = document.getElementById("option4");
+*/
+
 const startOption = document.getElementById("start");
 const startButtons = document.getElementsByClassName("startButtons");
 const allOptionButton = document.getElementsByClassName("optionButtons");
@@ -9,10 +14,14 @@ const gameSettings = document.getElementsByClassName("gameSettings");
 const retry = document.getElementById("retry");
 const questionButton = document.getElementById("question");
 
+buttons.forEach(it => $(it).hide())
+/*
 $(button1).hide();
 $(button2).hide();
 $(button3).hide();
 $(button4).hide();
+*/
+
 $(retry).hide();
 $(gameSettings).hide();
 
@@ -42,18 +51,28 @@ let score = 0
 let n = 0;
 console.log(typeof questions[n].options[0])
 console.log(questions[n].options[3])
+
 setUpQuestion = () =>{
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].innerHTML = questions[n].options[i]
+  }
+    /*
   button1.innerHTML = questions[n].options[0]
   button2.innerHTML = questions[n].options[1]
   button3.innerHTML = questions[n].options[2]
   button4.innerHTML = questions[n].options[3]
+    */
 };
 
 gameStart = () =>{
+    console.log('game start n', n)
+    buttons.forEach(it => $(it).show())
+    /*
     $(button1).show();
     $(button2).show();
     $(button3).show();
     $(button4).show();
+    */
     $(gameSettings).show();
     $(startOption).hide();
     setUpQuestion();
@@ -68,7 +87,12 @@ checkButton = (input) =>{
     ++n;
 }
 
-button1.addEventListener("click", checkButton(questions[n].options[0]));
-button2.addEventListener("click", checkButton(questions[n].options[1]));
-button3.addEventListener("click", checkButton(questions[n].options[2]));
-button4.addEventListener("click", checkButton(questions[n].options[3]));
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => checkButton(questions[n].options[i]))
+}
+/*
+button1.addEventListener("click", () => checkButton(questions[n].options[0]));
+button2.addEventListener("click", () => checkButton(questions[n].options[1]));
+button3.addEventListener("click", () => checkButton(questions[n].options[2]));
+button4.addEventListener("click", () => checkButton(questions[n].options[3]));
+*/
